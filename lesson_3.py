fae4e5bf4b3d88d839e9bb93f71aeb5fca73fbe9
pro32Vi17Qy <77281256+pro32Vi17Qy@users.noreply.github.com>
2022-05-16 16:53:14 +0300

    Add files via upload

---
+++ lesson_3.py
@@ -0,0 +1,89 @@
+class Hello_world:
+    hello = "Hello"
+    _hello = "_Hello"
+    __hello = "__Hello"
+
+    def __init__(self):
+        self.world = "World"
+        self._world = "_World"
+        self.__world = "__World"
+
+    def printer(self):
+        print(self.hello)
+        print(self._hello)
+        print(self.__hello)
+        print(self.world)
+        print(self._world)
+        print(self.__world)
+
+
+class Hi(Hello_world):
+
+    def hi_print(self):
+        print(self.hello)
+        print(self.world)
+        print(self._hello)
+        print(self._world)
+        print(self._Hi__hello)
+        print(self.__world)
+        hello = Hello_world()
+        hello.printer()
+
+
+hi = Hi()
+hi.hi_print()
+
+
+# class Human:
+#     weight = 70
+#     old = 80
+#     endurance = 100
+#     def __init__(self):
+#         print("Human")
+#         print(f"weight {self.weight}")
+#         print(f"old {self.old}")
+#         print(f"endurance {self.endurance}")
+# class elves(Human):
+#     weight = 20
+#     old = 500
+#     manna = 45
+#     def __init__(self):
+#         print("Human")
+#         print(f"weight {self.weight}")
+#         print(f"old {self.old}")
+#         print(f"endurance {self.endurance}")
+#         print(f"manna {self.manna}")
+# class hobbyt(Human):
+#     endurance = 50
+#     def __init__(self):
+#         print("Human")
+#         print(f"weight {self.weight}")
+#         print(f"old {self.old}")
+#         print(f"endurance {self.endurance}")
+#
+# a = Human()
+# b = elves
+# c = hobbyt
+
+
+
+
+
+
+
+
+
+
+
+
+# class Human:
+#     height = 170
+# class Stusent(Human):
+#     pass
+# class worker(Human):
+#     pass
+# Nick = Stusent
+# Ann = worker
+#
+# print(Nick.height)
+# print(Ann.height)

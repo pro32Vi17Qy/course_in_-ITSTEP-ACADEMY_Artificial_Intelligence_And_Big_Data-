63c71de3807eff510ea82f362b4e86761e7af7ee
pro32Vi17Qy <77281256+pro32Vi17Qy@users.noreply.github.com>
2022-05-16 16:56:01 +0300

    Add files via upload

---
+++ dz_4.py
@@ -0,0 +1,19 @@
+class Human:
+    endurance_min = 100
+    power_kN = 100
+    intelligence_IQ = 120
+    height_sm = 150
+    weight_kg = 60
+    skin_color = "Skin's color"
+    location = "Earth"
+class UNICO(Human):
+    intelligence_IQ = 160
+class Aliens(Human):
+    endurance_min = 200
+    power_kN = 15
+    intelligence_IQ = 500
+    height_sm = 75
+    weight_kg = 20
+    skin_color = "BLue"
+    location = "Space"
+    planeLight_m = 100

6a448d763b832baca2a90f283a604abc7282c6e3
pro32Vi17Qy <kriskovroman16072008@gmail.com>
2022-05-23 16:32:12 +0300

    create lesson_8

---
+++ lesson_8.py
@@ -0,0 +1,27 @@
+import random
+from colorama import Fore
+import datetime
+# a = random.randint(0, 100)
+# b = random.randint(100, 200)
+# c = random.randint(200, 300)
+# my_list = [a, b, c]
+# print(my_list)
+# i = iter(my_list)
+# for _ in my_list:
+#     print(next(i))
+
+class Counter:
+    def __init__(self, maxNum):
+        self.i = 0
+        self.maxNumber = maxNum
+    def __iter__(self):
+        self.i = 0
+        return self
+    def __next__(self):
+        self.i += 1
+        if self.i > self.maxNumber:
+            raise StopIteration
+        return self.i
+count = Counter(8)
+for element in count:
+    print(F"\n{Fore.RESET}{element}")

0ebd3a106584303661e66e73050b243a0768772a
pro32Vi17Qy <kriskovroman16072008@gmail.com>
2022-05-29 21:27:51 +0300

    create lesson_9

---
+++ lesson_9.py
@@ -0,0 +1,18 @@
+import logging
+
+# import os
+# path = os.mkdir("path")
+# file = open("path/file.txt", "w+")
+logging.basicConfig(level=logging.DEBUG, filename="logs.log", filemode="w+",
+                    format="\n\tWe have next logging massage:%(asctime)s%(levelname)s - %(message)s")
+
+logging.basicConfig(level=logging.DEBUG, filename="logs.log", filemode="w",
+                    format="We have next logging massage:%(asctime)s%(levelname)s - %(message)s")
+try:
+    print(5/0)
+except ZeroDivisionError:
+    logging.debug("debug")
+    logging.info("info")
+    logging.warning("warning")
+    logging.error("error")
+    logging.critical("critical")

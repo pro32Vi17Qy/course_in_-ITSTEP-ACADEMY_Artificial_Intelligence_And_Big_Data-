6541c6e50b69e71cede4733b997f1991b5c3c823
pro32Vi17Qy <kriskovroman16072008@gmail.com>
2022-05-29 21:33:19 +0300

    create lesson_5

--- lesson_5.py
+++ lesson_5.py
@@ -19,25 +19,35 @@
 # helW = HelloWorld()
 
 class Computer:
-    def __init__(self,model,*args,**kwargs):
+    def __init__(self, model, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.model = model
         self.memory = "128 GB"
+
     def calculate(self):
         print("Calculating...")
+
+
 class Display:
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.resolution = "4k"
+
     def display(self):
         print("I display the image on the screen...")
+
+
 class Phone:
     def call(self):
         print("calling...")
-class SmartPhone(Display,Computer,Phone):
+
+
+class SmartPhone(Display, Computer, Phone):
     def printInfo(self):
         print(self.model)
         print(self.resolution)
         print(self.memory)
-iphone = SmartPhone(model = "1.12.5")
+
+
+iphone = SmartPhone(model="1.12.5")
 iphone.printInfo()

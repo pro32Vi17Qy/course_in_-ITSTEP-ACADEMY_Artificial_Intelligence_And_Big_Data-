98ef043daf435e57053aa86365e004d37c17a9ab
pro32Vi17Qy <kriskovroman16072008@gmail.com>
2022-05-29 21:36:17 +0300

    create dz_5

--- dz_5.py
+++ dz_5.py
@@ -1,17 +1,24 @@
 class car:
-    def __init__(self, *args,**kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.brand = "BMW"
         self.price = "500000$"
+
+
 class machine:
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.engine = "Electric"
+
+
 class vehicle:
     pass
+
+
 class BMW(car, machine, vehicle):
     def car_info(self):
         print(f"\nCar info(BMW X10): \n\tBrand - {self.brand};\n\tPrice - {self.price};\n\tEngine - {self.engine}.")
+
 
 BMW_X10 = BMW()
 BMW_X10.car_info()

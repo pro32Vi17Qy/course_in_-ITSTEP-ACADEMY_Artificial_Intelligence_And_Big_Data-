e32ac5c838576823cc19c1ca98464e6a8bce8342
pro32Vi17Qy <kriskovroman16072008@gmail.com>
2022-05-30 15:34:04 +0300

    create dz_9

--- dz_9.py
+++ dz_9.py
@@ -21,34 +21,13 @@ try:
         f"from (-5 to 5) was not generated. happened, I welcome you to a list\n\t"
         f"in which each element of this progression"
         f" is divided by a random number from (-5 to 5) a little lower!")
-    d1 = range(0, 10)
-    it_d1 = iter(d1)
-    d2 = range(0, 10)
-    it_d2 = iter(d2)
+    d = range(0, 10)
+    it_d = iter(d)
 
-    random_value_division1 = random.randint(-5, 5)
-    random_value_division2 = random.randint(-5, 5)
-    random_value_division3 = random.randint(-5, 5)
-    random_value_division4 = random.randint(-5, 5)
-    random_value_division5 = random.randint(-5, 5)
-    random_value_division6 = random.randint(-5, 5)
-    random_value_division7 = random.randint(-5, 5)
-    random_value_division8 = random.randint(-5, 5)
-    random_value_division9 = random.randint(-5, 5)
-    random_value_division10 = random.randint(-5, 5)
-
-    a1 = [(1 + next(it_d1) * 5) for i1 in range(10)]
+    a1 = [(1 + next(it_d) * 5) for i1 in range(10)]
     print(f"\t\n\t{Fore.LIGHTCYAN_EX}List of arithmetic progression - {Fore.LIGHTWHITE_EX}{a1}")
-    a1[0] /= random_value_division1
-    a1[1] /= random_value_division2
-    a1[2] /= random_value_division3
-    a1[3] /= random_value_division4
-    a1[4] /= random_value_division5
-    a1[5] /= random_value_division6
-    a1[6] /= random_value_division7
-    a1[7] /= random_value_division8
-    a1[8] /= random_value_division9
-    a1[9] /= random_value_division10
+    for i in d:
+        a1[i] /= random.randint(-5, 5)
     print(f"\t\n\t{Fore.LIGHTCYAN_EX}A list in which each element of this progression is divided "
           f"by a random number from (-5 to 5) - {Fore.LIGHTWHITE_EX}{a1}")
 except ZeroDivisionError:

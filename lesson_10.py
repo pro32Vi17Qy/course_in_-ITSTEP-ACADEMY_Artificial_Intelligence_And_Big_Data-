5df707a752228165b0f6e8d7c98b921671eded15
pro32Vi17Qy <kriskovroman16072008@gmail.com>
2022-05-30 16:06:15 +0300

    create lesson_10 - part 2 of part_1

--- lesson_10.py
+++ lesson_10.py
@@ -1,5 +1,8 @@
-from colorama import Fore
+from colorama import Fore, Back
 import requests
 
-response = requests.get("https://httpbin.org/get")
-print(f"{Fore.LIGHTWHITE_EX}\n\t{response.content}")
+# response = requests.get("https://httpbin.org/get")
+# print(f"{Fore.LIGHTWHITE_EX}\n\t{response.text}")
+
+response = requests.post("https://httpbin.org/post", data="TEST DATA", headers = {"h1" : "Test title" })
+print(response.text)
